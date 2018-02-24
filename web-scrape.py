f85ead48a73162a169f9c0f27b09e6e5a7c7a771
SeyedHosseinTafakh <colonel.hossein@gmail.com>
2018-02-24 15:48:01 -0800

    second update

--- web-scrape.py
+++ web-scrape.py
@@ -1,48 +1,45 @@
-
+from asyncio import wait_for
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
-ff = webdriver.Chrome()
-ff.get("https://www.instagram.com/?hl=en")
-# ff.save_screenshot('pics/screenie.png')
-# loginButton = ff.find_element_by_class_name('_g9ean').a
-# loginButton.send_keys(Keys.RETURN)
 
-#element.send_keys('hossein')
 
-#element.send_keys(Keys.RETURN)
-#assert "No results found." not in ff.page_source
-#ff.close()
-
-element = ff.find_element_by_class_name('_g9ean')
-# print(element)
+ch = webdriver.Chrome()
+ch.get("https://www.instagram.com/?hl=en")
+element = ch.find_element_by_class_name('_g9ean')
 element = element.find_element_by_xpath('a')
 element.click()
-userName = ff.find_element_by_name('username')
-userName.send_keys('hossein__t_k')
-password = ff.find_element_by_name('password')
+userNameInput = ch.find_element_by_name('username')
+userNameInput.send_keys('shandool_paroon4')
+password = ch.find_element_by_name('password')
 
 
+password.send_keys('123456789h')
 
-
-
-
-
-
-
-
-
-
-
-
-
-password.send_keys('5260145143ho')
-button = ff.find_element_by_xpath('/html/body/span/section/main/article/div[2]/div[1]/div/form/span/button')
-#class_name = button.get_attribute('class')
-# button.find_element_by_xpath('button')
+button = ch.find_element_by_xpath('/html/body/span/section/main/article/div[2]/div[1]/div/form/span/button')
 button.click()
 
+element = WebDriverWait(ch, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div/div/span[2]')))
+print(element)
+userName = 'hossein__t_k'
+ch.get("https://www.instagram.com/" + userName + "/")
+followersButton = ch.find_element_by_xpath('//*[@id="react-root"]/section/main/article/header/section/ul/li[2]/a')
+followersNumber = ch.find_element_by_xpath('//*[@id="react-root"]/section/main/article/header/section/ul/li[2]/a/span')
+
+followersNumber  = followersNumber.get_attribute('title')
+followersButton.click()
+
+element = WebDriverWait(ch, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]/div/div[2]')))
+element = element.find_element_by_xpath('/html/body/div[4]/div/div[2]/div/div[2]/ul/div/li[1]/div/div[1]/div/div[1]/a')
+print(element.get_attribute('text'))
+
+ch.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 
 
